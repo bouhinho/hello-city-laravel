db53524de156f9e51d3284bf4e87618b19dc2f4b
bouh <bouhsilva@gmail.com>
2020-12-11 14:43:23 +0300

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,9 @@
 @section('title', 'About Us | ' . config('app.name'))
 
 @section('content')
+    
+    <img src="/images/tdn-logo.jpg" alt="TDN Logo">
+
     <p>Built with &hearts; by LES TEACHER DU NET.</p>
 
     <p><a href="{{ route('home')}} ">Revenir à la page d'accueil</a></p>    
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,9 @@
 @extends('app')
 
-@section('title', 'Hello City')
-
 @section('content')
+
+    <img src="/images/djibouti-flag.png" alt="Djibouti Flag">
+   
     <h1>Hello from Djibouti!</h1>
 
     <p>It's currently {{ date('h:i A') }}.</p>    
